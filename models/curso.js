fb27d41ec498cdf5340e53d3aa8e829128c24f25
Felipe Caiafa <felipe.soares@sou.inteli.edu.br>
2025-05-15 11:27:20 -0300

    a3

--- models/curso.js
+++ models/curso.js
@@ -6,6 +6,18 @@ module.exports = {
     return result.rows;
   },
 
+// Função no model para atualizar o nome de um curso pelo ID
+  async update(id, nome) {
+   const query = 'UPDATE curso SET nome = $1 WHERE id = $2 RETURNING *';
+   const result = await db.query(query, [nome, id]);
+   return result.rows[0];
+  },
+  
+  // Função no model que remove um curso do banco de dados pelo ID
+  async delete(id) {
+   await db.query('DELETE FROM curso WHERE id = $1', [id]);
+  },
+
   async create(nome) {
     const query = 'INSERT INTO curso (nome) VALUES ($1) RETURNING *';
     const result = await db.query(query, [nome]);
